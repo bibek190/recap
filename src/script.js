a28525e0ac67cbeee10534864a52dc9293a6cd37
bibek190 <bibek9818917036@gmail.com>
2024-10-21 23:54:20 +1100

    added

--- src/script.js
+++ src/script.js
@@ -1,12 +1,16 @@
 import gsap from "gsap";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const canvas = document.querySelector(".webgl");
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const geometry = new THREE.SphereGeometry(1, 16, 16);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
 
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -15,6 +19,18 @@ const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
+
+// resize
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -36,21 +52,19 @@ const cursor = {
 };
 
 //
-window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = -(event.clientY / sizes.height - 0.5);
-});
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-  //   console.log(elapsedTime);
 
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
-  camera.position.y = cursor.y * 3;
-  camera.lookAt(mesh.position);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  // camera.position.y = cursor.y * 3;
+  // camera.lookAt(mesh.position);
+  controls.update();
 
   renderer.render(scene, camera);
 
